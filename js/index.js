6d35b0fc0827bb561d7b786ed0a80eea3d8727fe
Faisal Aziz <faziz6006@gmail.com>
2019-12-02 16:49:56 -0600

    first sub

--- js/index.js
+++ js/index.js
@@ -40,3 +40,18 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// TOP PICTURE
+let header = document.getElementById("cta-img"); 
+header.setAttribute('src', siteContent["cta"]["img-src"])
+
+let middleImg = document.getElementById("middle-img"); 
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+
+//NAVigation BAR 
+let aTags = document.getElementsByTagName('a')
+Array.from(aTags).forEach((link, i) => {
+  link.textContent = siteContent["nav"][`nav-item-${i + 1}`];
+  link.style.color ="green";
+});
